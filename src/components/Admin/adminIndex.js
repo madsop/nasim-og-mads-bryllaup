5e635fe57670dbfa11f49b076fa2abf46c71e901
Mads Opheim <mads.opheim@gmail.com>
2026-05-30 23:58:14 +0200

    We don't need admin panel

--- src/components/Admin/adminIndex.js
+++
@@ -1,9 +0,0 @@
-/**
- * @file adminIndex.js
- * @description Exports all components used in the admin panel for easy imports in other files.
- * @author Emanuele Sgroi
- * @date 19 October 2024
- */
-
-export { default as Dashboard } from "./Dashboard";
-export { default as GuestManagement } from "./GuestManagement";
